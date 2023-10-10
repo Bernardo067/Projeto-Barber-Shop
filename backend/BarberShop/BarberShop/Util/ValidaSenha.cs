5c2c50323f7ada5c2bfd0d30397f5dbcf1406d63
Henrique Grosscklauss <henriquegrossdeveloper@gmail.com>
2023-10-10 19:37:51 -0300

    Implementa validação de senha

---
+++ backend/BarberShop/BarberShop/Util/ValidaSenha.cs
@@ -0,0 +1,18 @@
+﻿namespace BarberShop.Util
+{
+    public class ValidaSenha
+    {
+        public static bool ValidacaoSenha(string password)
+        {
+            // Definindo os requisitos para a senha
+            var hasMinimumLength = password.Length >= 8;
+            var hasUppercaseLetter = password.Any(char.IsUpper);
+            var hasLowercaseLetter = password.Any(char.IsLower);
+            var hasDigit = password.Any(char.IsDigit);
+            var hasSpecialCharacter = password.Any(ch => !char.IsLetterOrDigit(ch));
+
+            // Verifique se a senha atende a todos os requisitos
+            return hasMinimumLength && hasUppercaseLetter && hasLowercaseLetter && hasDigit && hasSpecialCharacter;
+        }
+    }
+}
