1613bd8b6ff5ffb2ce3572986cd1cfa8183fc76b
Henrique Grosscklauss <henriquegrossdeveloper@gmail.com>
2023-10-10 18:07:12 -0300

    Cria Login e User models

---
+++ backend/BarberShop/BarberShop/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BarberShop.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required]
+        public string? Nome { get; set; }
+        [Required]
+        public string? SobreNome { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        public TipoUsuario TipoUsuario { get; set; }
+
+    }
+
+    public enum TipoUsuario
+    {
+        Client,
+        Admin,
+        Barber
+    }
+}
+
