face06c4bf870cd812ea213516155b7eab32f06e
Henrique Grosscklauss <henriquegrossdeveloper@gmail.com>
2023-10-10 19:37:20 -0300

    Ajusta DbContext

--- backend/BarberShop/BarberShop/Data/ApplicationDbContext.cs
+++ backend/BarberShop/BarberShop/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BarberShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BarberShop.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
