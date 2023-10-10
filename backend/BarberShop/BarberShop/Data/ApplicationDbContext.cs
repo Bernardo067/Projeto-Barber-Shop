c3b6e8d0c0674ea8bb676aad1b29d18f2740df99
Henrique Grosscklauss <henriquegrossdeveloper@gmail.com>
2023-10-10 18:14:14 -0300

    Configura DbContext

---
+++ backend/BarberShop/BarberShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BarberShop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Access IConfiguration through ServiceProvider
+                var serviceProvider = new ServiceCollection()
+                    .AddSingleton<IConfiguration>(provider =>
+                    {
+                        var serviceScope = provider.GetRequiredService<IServiceScopeFactory>().CreateScope();
+                        return serviceScope.ServiceProvider.GetRequiredService<IConfiguration>();
+                    })
+                    .BuildServiceProvider();
+
+                var configuration = serviceProvider.GetService<IConfiguration>();
+                string connectionString = configuration["ConnectionStrings:DefaultSQLConnection"];
+
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
